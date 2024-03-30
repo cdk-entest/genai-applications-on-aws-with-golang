da478b4c27ff710607a2a4a70daeb5c30cc47ded
haitran <hai@entest.io>
2024-03-30 09:37:49 +0000

    test

--- build.py
+++ build.py
@@ -8,8 +8,8 @@ ACCOUNT = os.environ["ACCOUNT_ID"]
 # delete all docker images
 os.system("sudo docker system prune -a")
 
-# build go-blog-app image
-os.system("sudo docker build -t go-blog-app . ")
+# build go-bedrock-app image
+os.system("sudo docker build -t go-bedrock-app . ")
 
 #  aws ecr login
 os.system(
@@ -17,25 +17,25 @@ os.system(
 )
 
 # get image id
-IMAGE_ID = os.popen("sudo docker images -q go-blog-app:latest").read()
+IMAGE_ID = os.popen("sudo docker images -q go-bedrock-app:latest").read()
 
-# tag go-blog-app image
+# tag go-bedrock-app image
 os.system(
-    f"sudo docker tag {IMAGE_ID.strip()} {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/go-blog-app:latest"
+    f"sudo docker tag {IMAGE_ID.strip()} {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/go-bedrock-app:latest"
 )
 
 # create ecr repository
 os.system(
-    f"aws ecr create-repository --registry-id {ACCOUNT} --repository-name go-blog-app --region {REGION}"
+    f"aws ecr create-repository --registry-id {ACCOUNT} --repository-name go-bedrock-app --region {REGION}"
 )
 
 # push image to ecr
 os.system(
-    f"sudo docker push {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/go-blog-app:latest"
+    f"sudo docker push {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/go-bedrock-app:latest"
 )
 
 # run locally to test
-# os.system(f"sudo docker run -d -p 3001:3000 go-blog-app:latest")
+# os.system(f"sudo docker run -d -p 3001:3000 go-bedrock-app:latest")
 
 
 # def update_service():
