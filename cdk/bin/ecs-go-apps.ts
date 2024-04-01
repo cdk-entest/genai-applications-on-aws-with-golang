49a849d5045d05b2c505e02acb8af28d67678ebb
haitran <hai@entest.io>
2024-04-01 03:50:05 +0000

    test

---
+++ cdk/bin/ecs-go-apps.ts
@@ -0,0 +1,64 @@
+import * as cdk from "aws-cdk-lib";
+import { EcsClusterStack } from "../lib/ecs-cluster";
+import {
+  VPC_ID,
+  VPC_NAME,
+  REGION,
+  AOSS_ARN,
+  ECR_REPO_NAME,
+  GO_BLOG_ACM_CERT_ARN,
+  BUCKET_NAME,
+  BUCKET_ARN,
+  ARN_PRINCIPAL_ACCESS_AOSS,
+  AOSS_DOMAIN,
+  AOSS_COLLECTION_ARN,
+  AOSS_INDEX_NAME,
+} from "../config";
+import { GoBedrockService } from "../lib/service-go-bedrock";
+import { AOSSStack } from "../lib/aoss";
+import { LambdaAossStack } from "../lib/lambda-aoss";
+
+const ACCOUNT = process.env.CDK_DEFAULT_ACCOUNT;
+
+// create cdk app
+const app = new cdk.App();
+
+// create amazon opensearch collection
+const aoss = new AOSSStack(app, "AmazonOpenSearchStack", {
+  name: "demo",
+  arnPrincipal: ARN_PRINCIPAL_ACCESS_AOSS,
+});
+
+// create lambda indexing aoss collection
+new LambdaAossStack(app, "LambdaAossBedrockStack", {
+  opensearchDomain: AOSS_DOMAIN,
+  aossCollectionArn: AOSS_COLLECTION_ARN,
+  bucketName: BUCKET_NAME,
+  aossIndexName: AOSS_INDEX_NAME,
+});
+
+// create ecs cluster
+const cluster = new EcsClusterStack(app, "EcsClusterStack", {
+  vpcId: VPC_ID,
+  vpcName: VPC_NAME,
+  env: {
+    region: REGION,
+    account: ACCOUNT,
+  },
+});
+
+// create a go bedrock app
+new GoBedrockService(app, "GoBedrockService", {
+  cluster: cluster.cluster,
+  vpcId: VPC_ID,
+  vpcName: VPC_NAME,
+  // for https
+  certificate: "",
+  ecrRepoName: ECR_REPO_NAME,
+  aossCollectionArn: AOSS_ARN,
+  bucketArn: BUCKET_ARN,
+  env: {
+    region: REGION,
+    account: ACCOUNT,
+  },
+});
